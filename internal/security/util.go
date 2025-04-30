792e5489164e10b36ed93f5ff373a43415733b7b
dzherb <zherbin.dima@yandex.ru>
2025-05-01 01:55:12 +0300

    chore: remove magic number

--- internal/security/util.go
+++ internal/security/util.go
@@ -2,6 +2,8 @@ package security
 
 import "time"
 
+const nanosInSecond = 1e9
+
 func timeToFloat64(t time.Time) float64 {
-	return float64(t.UTC().UnixNano()) / 1e9
+	return float64(t.UTC().UnixNano()) / nanosInSecond
 }

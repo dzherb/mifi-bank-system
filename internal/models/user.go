efa4ff9b54c89ed3ff0a13515260d40f7d5e7ffb
dzherb <zherbin.dima@yandex.ru>
2025-05-01 23:42:23 +0300

    feat: remove password field from serialization

--- internal/models/user.go
+++ internal/models/user.go
@@ -6,7 +6,7 @@ type User struct {
 	ID        int       `json:"id"`
 	Email     string    `json:"email"`
 	Username  string    `json:"username"`
-	Password  string    `json:"password"`
+	Password  string    `json:"-"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }

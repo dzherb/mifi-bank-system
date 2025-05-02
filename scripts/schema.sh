524c02815337daeaec2c57cb098ccd1347ac8afe
Dmitrii Zherbin <zherbin.dima@yandex.ru>
2025-05-02 19:57:49 +0300

    refactor: simplify schema script

--- scripts/schema.sh
+++ scripts/schema.sh
@@ -3,33 +3,22 @@ set -e
 
 PROJECT_ROOT="$(dirname "$(realpath "$0")")/../"
 
-source "$PROJECT_ROOT/.env"
+export $(grep DATABASE_URL "$PROJECT_ROOT/.env")
 
-# extract the protocol
-proto="`echo $DATABASE_URL | grep '://' | sed -e's,^\(.*://\).*,\1,g'`"
-# remove the protocol
-url=`echo $DATABASE_URL | sed -e s,$proto,,g`
+eval $(python3 - <<EOF
+import os
+from urllib.parse import urlparse
 
-# extract the user and password (if any)
-userpass="`echo $url | grep @ | cut -d@ -f1`"
-pass=`echo $userpass | grep : | cut -d: -f2`
-if [ -n "$pass" ]; then
-    user=`echo $userpass | grep : | cut -d: -f1`
-else
-    user=$userpass
-fi
-
-# extract the host -- updated
-hostport=`echo $url | sed -e s,$userpass@,,g | cut -d/ -f1`
-port=`echo $hostport | grep : | cut -d: -f2`
-if [ -n "$port" ]; then
-    host=`echo $hostport | grep : | cut -d: -f1`
-else
-    host=$hostport
-fi
-
-# extract the db name
-dbname="`echo $url | grep / | cut -d/ -f2-`"
+p = urlparse(os.environ["DATABASE_URL"])
+print(f"""
+user={p.username}
+pass={p.password}
+host={p.hostname}
+port={p.port or 5432}
+dbname={p.path[1:]}
+""")
+EOF
+)
 
 export PGPASSWORD=$pass
 

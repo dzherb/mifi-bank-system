bc088950f82752544c9c7a854787b83d7db82416
Dmitrii Zherbin <zherbin.dima@yandex.ru>
2025-05-02 13:08:43 +0300

    chore: set up db tracer

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/golang-migrate/migrate/v4 v4.18.3
 	github.com/google/uuid v1.6.0
 	github.com/gorilla/mux v1.8.1
+	github.com/jackc/pgx-logrus v0.0.0-20220919124836-b099d8ce75da
 	github.com/jackc/pgx-shopspring-decimal v0.0.0-20220624020537-1d36b5a1853e
 	github.com/jackc/pgx/v5 v5.7.4
 	github.com/joho/godotenv v1.5.1
--- internal/storage/postgres.go
+++ internal/storage/postgres.go
@@ -4,41 +4,15 @@ import (
 	"context"
 
 	"github.com/dzherb/mifi-bank-system/internal/config"
+	"github.com/jackc/pgx-logrus"
 	"github.com/jackc/pgx-shopspring-decimal"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/tracelog"
+	log "github.com/sirupsen/logrus"
 )
 
 var pool *pgxpool.Pool
-
-func Init(cfg *config.Config) (func(), error) {
-	var err error
-
-	pgxCfg, err := pgxpool.ParseConfig(cfg.DatabaseURL)
-	if err != nil {
-		return nil, err
-	}
-
-	pgxCfg.AfterConnect = registerTypes
-
-	pool, err = pgxpool.NewWithConfig(context.Background(), pgxCfg)
-
-	if err != nil {
-		return nil, err
-	}
-
-	err = pool.Ping(context.Background())
-	if err != nil {
-		return nil, err
-	}
-
-	return ClosePool, nil
-}
-
-func registerTypes(_ context.Context, conn *pgx.Conn) error {
-	decimal.Register(conn.TypeMap())
-	return nil
-}
 
 func Pool() *pgxpool.Pool {
 	if pool == nil {
@@ -57,4 +31,42 @@ func ClosePool() {
 
 var Conn = func() Connection { // can be mocked in tests
 	return Pool()
+}
+
+func Init(cfg *config.Config) (func(), error) {
+	var err error
+
+	pgxCfg, err := pgxpool.ParseConfig(cfg.DatabaseURL)
+	if err != nil {
+		return nil, err
+	}
+
+	pgxCfg.ConnConfig.Tracer = logrusTracer(tracelog.LogLevelWarn)
+	pgxCfg.AfterConnect = registerTypes
+
+	pool, err = pgxpool.NewWithConfig(context.Background(), pgxCfg)
+	if err != nil {
+		return nil, err
+	}
+
+	err = pool.Ping(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	return ClosePool, nil
+}
+
+func registerTypes(_ context.Context, conn *pgx.Conn) error {
+	decimal.Register(conn.TypeMap())
+	return nil
+}
+
+func logrusTracer(level tracelog.LogLevel) *tracelog.TraceLog {
+	logger := logrus.NewLogger(log.StandardLogger())
+
+	return &tracelog.TraceLog{
+		Logger:   logger,
+		LogLevel: level,
+	}
 }
